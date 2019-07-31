8ef2874f0a744bb9e6aae9015090fdd4e7e198f4
Bamdad Dashtban <bamdadd@gmail.com>
2019-07-31 16:29:43 +0100

    New Release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b-social/ping-resource "0.0.0"
+(defproject b-social/ping-resource "0.0.1"
   :description "FIXME: write description"
   :url "https://github.com/B-Social/ping-resource"
   :license {:name "Eclipse Public License"
